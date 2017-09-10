c75ba2781344c04c37d2628251150d96c3934be5
Trent <trentfromrid@gmail.com>
2017-09-10 00:03:12 -0600

    Fixed default config file path.

--- WUDownloader/Configuration.cs
+++ WUDownloader/Configuration.cs
@@ -60,7 +60,7 @@ namespace WUDownloader
             else
             {
                 RootFolderPath = "C:\\WUDownloader";
-                ConfigurationFolderPath = rootFolderPath + "config.txt";
+                ConfigurationFolderPath = rootFolderPath + "\\config.txt";
             }
             
             DownloadFolderPath = rootFolderPath + "\\Downloads";
